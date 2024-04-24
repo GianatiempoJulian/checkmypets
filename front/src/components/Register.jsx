92832369ac661aba94838749f389f10c684c3fd0
GianatiempoJulian <gianatiempojulian@gmail.com>
2024-04-23 23:55:07 -0300

    0.4.2 | Installed react-hook-form. I've started test with that.

--- front/src/components/Register.jsx
+++ front/src/components/Register.jsx
@@ -1,59 +1,78 @@
 import { useState } from "react";
 import { Button } from "./Button"
+import { useForm } from "react-hook-form"
 
 export function Register () {
-    const [name, setName] = useState(" ");
-    const [lastname, setLastname] = useState(" ");
-    const [address, setAddress] = useState(" ")
-    const [phoneNumber, setPhoneNumber] = useState(" ")
-    const [email, setEmail] = useState(" ");
-    const [password, setPassword] = useState(" ");
+  const {register, handleSubmit, 
+    formState:{errors}
+  } = useForm()
+
+
+
+
+  const onSubmit = handleSubmit((data) =>{
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({data})
+  };
+
+    fetch("http://localhost:5000/api/user/register", requestOptions)
+      .then(response => response.json())
+      .then(a => console.log(a));
+  })  
 
     return (
       <>
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={onSubmit}>
             <input 
             type="text" 
-            value={name} 
             name="name" 
-            onChange={target => setName(target.value)}
-            className="m-2 text-black"
-            />
+            {...register("name",{
+              required: true
+            },)}
+            className="m-2 text-black" 
+            />{errors.name && <span className="text-sm text-red-700 ml-5">Nombre es requerido.</span>}
             <input 
             type="text" 
-            value={lastname} 
+            {...register("lastname",{
+              required: true
+            })}
             name="lastname"
-            onChange={target => setLastname(target.value)}
             className="m-2 text-black"
-            />
+            />{errors.lastname && <span className="text-sm text-red-700 ml-5">Apellido es requerido.</span>}
             <input 
             type="text" 
-            value={address} 
-            name="address" 
-            onChange={target => setAddress(target.value)}
+            name="address"
+            {...register("address",{
+              required: true
+            })}
             className="m-2 text-black"
-            />
+            />{errors.address && <span className="text-sm text-red-700 ml-5">Dirección es requerido.</span>}
             <input 
             type="number" 
-            value={phoneNumber} 
-            name="phoneNumber" 
-            onChange={target => setPhoneNumber(target.value)}
+            name="phoneNumber"
+            {...register("phoneNumber",{
+              required: true
+            })}
             className="m-2 text-black"
-            />
+            />{errors.phoneNumber && <span className="text-sm text-red-700 ml-5">Teléfono es requerido.</span>}
             <input 
             type="email" 
-            value={email} 
-            name="email" 
-            onChange={target => setEmail(target.value)}
+            name="email"
+            {...register("email",{
+              required: true
+            })}
             className="m-2 text-black"
-            />
+            />{errors.email && <span className="text-sm text-red-700 ml-5">Email es requerido.</span>}
             <input
             type="password" 
-            value={password} 
-            name="password" 
-            onChange={target => setPassword(target.value)} 
+            name="password"
+            {...register("password",{
+              required: true
+            })}
             className="m-2 text-black"
-            />
+            />{errors.password && <span className="text-sm text-red-700 ml-5">Contraseña es requerido.</span>}
             <Button text="Register"></Button>
          </form>
       </>
